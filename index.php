cda35c54c2726ecca470ae988b8966264ea78f2d
fabioschneider2009 <fabioschneider2009@hotmail.com>
2018-08-13 22:22:44 -0300

    Admin - Categorias

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -222,6 +223,94 @@ $app->post('/admin/forgot/reset', function() {
 	]);
 
 	$page->setTpl("forgot-reset-success");
+
+});
+
+// Rotas para Categorias
+$app->get("/admin/categories", function() {
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", array(
+		"categories"=>$categories
+	));
+
+});
+
+$app->get('/admin/categories/create', function() {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->get('/admin/categories/:idcategory/delete', function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get('/admin/categories/:idcategory', function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", array(
+		"category"=>$category->getValues()
+	));
+
+});
+
+$app->post('/admin/categories/create', function() {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+	
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->post('/admin/categories/:idcategory', function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
 
 });
 
